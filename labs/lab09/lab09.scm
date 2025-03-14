4e8f67e6eadcf797f3bcc5c3511443ef68dde810
talentestors <talentestors@gmail.com>
2025-03-14 21:06:41 +0800

    Day 26: lab09

--- labs/lab09/lab09.scm
+++ labs/lab09/lab09.scm
@@ -1,30 +1,28 @@
 (define (over-or-under num1 num2)
-    ; first of ways
-    ; (if(> num1 num2)
-    ;     1
-    ;     (if(< num1 num2)
-    ;         -1
-    ;         0))
-    (cond
+  ; first of ways
+  ; (if (> num1 num2)
+  ;     1
+  ;     (if (< num1 num2)
+  ;         -1
+  ;         0))
+  (cond
     ((> num1 num2) 1)
     ((< num1 num2) -1)
-    (else 0)
-    ))
+    (else 0)))
 
-(define (make-adder num) 
-    (lambda (inc) (+ num inc))
-)
+(define (make-adder num)
+  (lambda (inc)
+    (+ num inc)))
 
-(define (composed f g) 
-    (lambda (x) (f(g x)))
-)
+(define (composed f g)
+  (lambda (x)
+    (f (g x))))
 
-(define (repeat f n) 
-    (if(= n 0)
-        (lambda (x) x)
-        (composed f (repeat f (- n 1)))
-    )
-)
+(define (repeat f n)
+  (if (= n 0)
+      (lambda (x)
+        x)
+      (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -36,11 +34,9 @@
       b
       a))
 
-(define (gcd a b) 
-    (define c (max a b))
-    (define d (min a b))
-    (if (zero? (modulo c d))
-        d
-        (gcd d (modulo c d))
-    )
-)
+(define (gcd a b)
+  (define c (max a b))
+  (define d (min a b))
+  (if (zero? (modulo c d))
+      d
+      (gcd d (modulo c d))))
---
+++ note/exam-program/ex15/ex.scm
@@ -0,0 +1,20 @@
+;;; non-empty subsets of integer list s that have an even sum
+(define (even-subsets s)
+  if (null? s) nil
+  (append (even-subsets (cdr s))
+            (subset-helper even? s))))
+
+;;; non-empty subsets of integer list s that have an odd sum
+(define (odd-subsets s)
+  if (null? s) nil
+  (append (odd-subsets (cdr s))
+            (subset-helper odd? s))))
+
+（define (subset-helper f s)
+    (append 
+      (map (lambda (t) (cons (car s) t))
+           (if (odd? (car s))
+             (even-subsets (cdr s))
+             (odd-subsets (cdr s))))
+      (if (odd? (car s)) (list (list (car s))) nil))))
+
