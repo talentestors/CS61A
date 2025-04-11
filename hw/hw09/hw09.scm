a5623fe02c9e73d5830f2bd1b2d31a2b59c1e36a
talentestors <talentestors@gmail.com>
2025-04-11 17:31:42 +0800

    Day 31: hw09

--- hw/hw09/hw09.scm
+++ hw/hw09/hw09.scm
@@ -1,31 +1,21 @@
-(define (curry-cook formals body) 
+(define (curry-cook formals body)
   (if (null? formals)
       body
-      (let ((first-formal (car formals))
-            (rest-body (curry-cook (cdr formals) body)))
-        `(lambda (,first-formal) ,rest-body)))
-)
+      `(lambda (,(car formals))
+         ,(curry-cook (cdr formals) body))))
 
 (define (curry-consume curry args)
   (if (null? args)
       curry
-      (curry-consume (curry (car args)) (cdr args))
-  )
-)
+      (curry-consume (curry (car args)) (cdr args))))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
 
 (define (switch-to-cond switch-expr)
-  (cons 'cond (map 
-        (lambda (option) (cons `(equal? ,(car (cdr switch-expr)) ,(car option)) 
-                            (cdr option)
-                          )) 
-        (car (cdr (cdr switch-expr)))))
-)
-
-; (define (switch-to-cond switch-expr)
-;   (cons 'cond    (map
-;       (lambda (option) (cons `(equal? ,(car (cdr switch-expr)) ,(car option)) (cdr option)))      
-;       (car (cdr (cdr switch-expr)))))
-; )
+  (cons 'cond
+        (map (lambda (option)
+               (cons
+                `(equal? ,(car (cdr switch-expr)) ,(car option))
+                (cdr option)))
+             (car (cdr (cdr switch-expr))))))
